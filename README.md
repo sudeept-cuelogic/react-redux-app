d4971252c2560a7cc39596eed8f2137d8ab13af6
sudeept-cuelogic <sudeep.tarlekar@cuelogic.com>
2017-12-18 12:50:08 +0530

    Added desc to readme.

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 
 - Clone This repo
 - Run `npm install` to install dependent node modules
-- Run webpack development server using `npm run dev`
+- Run webpack development server using `npm run dev`, make sure no other service is using port `8080`.
+- Visit `localhost:8080` in browser
--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 
+import store from './store';
 import App from './containers/App';
 
 ReactDOM.render(
-    <App />, document.getElementById('app')
+    <Provider>
+        <App />
+    </Provider>,
+    document.getElementById('app')
 );
