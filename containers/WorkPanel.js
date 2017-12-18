ab463537a05ab2f11434a7412828c7173e37ca66
sudeept-cuelogic <sudeep.tarlekar@cuelogic.com>
2017-12-18 20:05:44 +0530

    Added key for dynamically generated table rows.

--- containers/WorkPanel.js
+++ containers/WorkPanel.js
@@ -5,18 +5,20 @@ export default class WorkPanel extends React.Component {
         return(
             <div>
                 <table>
-                    <tr>
-                        <th>Username</th>
-                        <th>Email</th>
-                        <th>Age/Gender</th>
-                    </tr>
-                    {this.props.usersList.map((user, index) => {
-                        return (<tr >
-                            <td>{user.username}</td>
-                            <td>{user.email}</td>
-                            <td>{user.age} / {user.gender}</td>
-                        </tr>)
-                    })}
+                    <tbody>
+                        <tr>
+                            <th>Username</th>
+                            <th>Email</th>
+                            <th>Age/Gender</th>
+                        </tr>
+                        {this.props.usersList.map((user, index) => {
+                            return (<tr key={`row_${index}`}>
+                                <td>{user.username}</td>
+                                <td>{user.email}</td>
+                                <td>{user.age} / {user.gender}</td>
+                            </tr>)
+                        })}
+                    </tbody>
                 </table>
             </div>
         );
