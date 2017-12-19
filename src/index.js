aed7e52fadf7dedbf5ec3379ba477f8c65dd62d1
sudeept-cuelogic <sudeep.tarlekar@cuelogic.com>
2017-12-19 20:02:50 +0530

    Changed extension from jsx to js.

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { Provider } from 'react-redux';
 import store from './store';
 import { HashRouter, Route, Switch } from 'react-router-dom';
 
-import App from './containers/App.jsx';
+import App from './containers/App';
 import Session from './containers/Session';
 import UserDashboard from './containers/UserDashboard';
 
@@ -13,7 +13,7 @@ ReactDOM.render(
     <Provider store={store}>
         <HashRouter>
             <Switch>
-                <Route path="/" component={App} />
+                <Route path='/' component={App} />
                 <Route path='/users' component={UserDashboard} />
                 <Route path='/login' component={Session} />
             </Switch>
