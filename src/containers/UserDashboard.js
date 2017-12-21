7d5bca20a9db4f281a102968ade7d50322df3466
sudeept-cuelogic <sudeep.tarlekar@cuelogic.com>
2017-12-21 12:51:09 +0530

    Added esling plugin and removed eslint errors from code.

--- src/containers/UserDashboard.js
+++ src/containers/UserDashboard.js
@@ -1,55 +1,48 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import Header from '../components/Header';
-import NavPanel from '../components/NavPanel'
-import WorkPanel from './WorkPanel';
+
 import { logOut, changeNameInSession } from '../actions/sessionActions';
 import { updateUserList } from '../actions/userActions';
+import Header from '../components/Header';
+import NavPanel from '../components/NavPanel';
+import WorkPanel from './WorkPanel';
 
 const mapDispatchToProps = dispatch => ({
-    onLogOut: () => dispatch(logOut()),
-    onUsernameChanged: (newUsername, newList) => {
-        dispatch(changeNameInSession(newUsername))
-        dispatch(updateUserList(newList))
-    }
-
-})
+  onLogOut: () => dispatch(logOut()),
+  onUsernameChanged: (newUsername, newList) => {
+    dispatch(changeNameInSession(newUsername));
+    dispatch(updateUserList(newList));
+  }
+});
 
 class UserDashboard extends React.Component {
-    constructor(props) {
-        super(props);
-        this.handleLogout = this._handleLogout.bind(this);
-        this.handleUsernameChange = this._handleUsernameChange.bind(this);
-    }
+  _getNewUsersList (oldUsername, newUsername) {
+    const newUsersList = [...this.props.user.list];
+    const newUser = newUsersList.filter((user, index) => { user['index'] = index; return user.username === oldUsername })[0];
+    newUser.username = newUsername;
+    newUsersList.splice(newUser.index, 1, newUser);
+    return newUsersList;
+  }
 
-    _getNewUsersList = (oldUsername, newUsername) => {
-        let newUsersList = [ ...this.props.user.list ];
-        let newUser = newUsersList.filter((user, index) => {user['index'] = index; return user.username === oldUsername})[0];
-        newUser['username'] = newUsername;
-        newUsersList.splice(newUser.index, 1, newUser);
-        return newUsersList;
-    }
+  _handleLogout () {
+    this.props.onLogOut();
+    this.props.history.replace('login');
+  }
 
-    _handleLogout = () => {
-        this.props.onLogOut();
-        this.props.history.replace('login');
-    }
+  _handleUsernameChange (oldUsername, newUsername) {
+    const newUsersList = this._getNewUsersList(oldUsername, newUsername);
+    this.props.onUsernameChanged(newUsername, newUsersList);
+  }
 
-    _handleUsernameChange = (oldUsername, newUsername) => {
-        let newUsersList = this._getNewUsersList(oldUsername, newUsername)
-        this.props.onUsernameChanged(newUsername, newUsersList);
-    }
-
-    render() {
-
-        return(
-            <div id="userDashboard">
-                <Header handleLogout={this.handleLogout}/>
-                <NavPanel username={this.props.session.user} handleUsernameChange={this.handleUsernameChange} />
-                <WorkPanel usersList={this.props.user.list} />
-            </div>
-        );
-    }
+  render () {
+    return (
+      <div id="userDashboard">
+        <Header handleLogout={this.handleLogout} />
+        <NavPanel username={this.props.session.user} handleUsernameChange={this.handleUsernameChange} />
+        <WorkPanel usersList={this.props.user.list} />
+      </div>
+    );
+  }
 }
 
 export default connect(state => state, mapDispatchToProps)(UserDashboard);
--- src/reducers/sessionReducer.js
+++ src/reducers/sessionReducer.js
@@ -1,19 +1,19 @@
 import { LOGIN, LOGOUT, CHANGE_USERNAME } from '../constants/actionTypes';
 
 const initialState = {
-    user: '',
-    signedInAt: null
-}
+  user: '',
+  signedInAt: null
+};
 
 export default (state = initialState, action) => {
-    switch(action.type) {
-        case LOGIN :
-            return { ...state, user: action.user, signedInAt: action.signedInAt }
-        case LOGOUT :
-            return { ...state, user: '', signedInAt: null }
-        case CHANGE_USERNAME :
-            return { ...state, user: action.newName }
-        default :
-            return state
-    }
-}
+  switch (action.type) {
+    case LOGIN:
+      return { ...state, user: action.user, signedInAt: action.signedInAt };
+    case LOGOUT:
+      return { ...state, user: '', signedInAt: null };
+    case CHANGE_USERNAME:
+      return { ...state, user: action.newName };
+    default:
+      return state;
+  }
+};
