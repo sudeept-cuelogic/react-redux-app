66549df4dbbe464f89904a8ae3e7dd68993890d6
sudeept-cuelogic <sudeep.tarlekar@cuelogic.com>
2017-12-22 13:54:58 +0530

    Removed eslint errors.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import store from './store';
 import App from './containers/App';
-import MyProfile from './containers/MyProfile';
-import Session from './containers/Session';
-import UserDashboard from './containers/UserDashboard';
 
 ReactDOM.render(
     <Provider store={store}>
